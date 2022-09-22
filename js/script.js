02c638b342f5309b1ab290308a82f42d4d2964b0
Alexa Ped <alexaped16@gmail.com>
2022-09-22 18:47:43 -0500

    bootstrap changes

--- js/script.js
+++ js/script.js
@@ -1,30 +1,60 @@
-console.clear();
-const navSlide= ()=> {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks=document.querySelectorAll('.nav-links li');
- //Toggle Nav
-    
-    burger.addEventListener('click', () =>{
-        nav.classList.toggle('nav-active');
-    });
+/* toggle hamburger */
 
-    navLinks.forEach((link,index) =>{
-       if(link.style.animation) {
-           link.style.animation=''
-        } else {
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".nav-menu");
 
-            link.style.animation=`navLinkFade 0.5s ease forwards ${index /7 + 1.5}s`;
-        }
-        console.log(index / 7);
-    });
+function toggleNavMenu() {
+    if (menu.classList.contains("active")) {
+        menu.classList.remove("active");
+        /* add hamburger icon */
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+    } else {
+        menu.classList.add("active");
+
+        /* add X icon */
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+    }
 }
-navSlide();
 
+/* event listen */
+toggle.addEventListener("click", toggleNavMenu, false);
 
+/* drop down menu */
 
+const items = document.querySelectorAll(".item");
 
+function toggleItem() {
+    if (this.classList.contains("drop-down-menu-active")) {
+        this.classList.remove("drop-down-menu-active");
+    } else {
+        this.classList.add("drop-down-menu-active");
+    }
+}
 
+/* drop down event listen */
+for (let item of items) {
+    if (item.querySelector(".drop-down-menu")) {
+        item.addEventListener("click", toggleItem, false);
+        item.addEventListener("keypress", toggleItem, false);
+    }
+}
 
+/* drop down event listen */
+for (let item of items) {
+    if (item.querySelector(".drop-down-menu")) {
+        item.addEventListener("click", toggleItem, false);
+        item.addEventListener("keypress", toggleItem, false);
+    }
+}
 
+/* close drop down from clicking anywhere */
+function closeDropDown (event) {
+    let isClickInside = menu.contains(event.target);
 
+    if (!isClickInside && menu.querySelector(".drop-down-menu-active")) {
+        menu.querySelector(".drop-down-menu-active").classList.remove("drop-down-menu-active");
+    }
+}
+
+/* Listener */
+document.addEventListener("click", closeDropDown, false);
